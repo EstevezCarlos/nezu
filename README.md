e0c3622e65326bdd5eb9444c5387933376f70dd0
Chuck Wheeler <archdoodle@gmail.com>
2026-06-08 23:01:00 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nezu
 Minimal JS library to create web components quickly.
+
+## Minified with
+https://minify-js.com/
---
+++ nezu.global.js
@@ -0,0 +1,43 @@
+function def(name, template) {
+    const observedAttributes = [
+        ...new Set(
+            [...template.matchAll(/{{([^\s{}]+)}}/g)]
+                .map(match => match[1])
+        )
+    ]
+
+    customElements.define(name, class Component extends HTMLElement {
+        static get observedAttributes() {
+            return observedAttributes
+        }
+
+        constructor() {
+            super()
+            this.attachShadow({ mode: "open" })
+        }
+
+        connectedCallback() {
+            this.render()
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (oldValue !== newValue) {
+                this.render()
+            }
+        }
+
+        render() {
+            let html = template
+
+            for (const attributeName of observedAttributes) {
+                html = html.replaceAll(
+                    `{{${attributeName}}}`,
+                    this.getAttribute(attributeName) ?? ""
+                )
+            }
+
+            this.shadowRoot.innerHTML = html
+        }
+    })
+}
+
